34ad645116e379162006557fcbf61f10f9a0c80b
Ramazan774 <ramazan.ramazanov@glion.ch>
2021-02-25 19:41:26 -0800

    three cases

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,18 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {addItem, deleteItem} from './redux/actions';
 
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default App;
+
 
